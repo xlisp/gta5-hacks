79546e77c63b91d0d699369af1af6c9eb1c17870
Steve Chan <chanshunli@gmail.com>
2024-12-20 12:01:32 +0800

    Run in VS fine.

--- memory_modifier.cpp
+++ memory_modifier.cpp
@@ -1,3 +1,13 @@
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
+
 #include <windows.h>
 #include <tlhelp32.h>
 #include <iostream>
@@ -49,16 +59,30 @@ int main() {
 
     if (!processID) {
         std::wcerr << L"未找到目标进程。" << std::endl;
-        return 1;
+        //return 1;
+        std::cout << "no found pid!\n";
     }
 
     LPCVOID targetAddress = (LPCVOID)0x00007FF636AC859B; // 示例地址
-    BYTE newData[] = {0x48, 0x8B, 0x42, 0x08, 0xF6, 0x40, 0x38, 0x01}; // 示例新数据
+    BYTE newData[] = { 0x48, 0x8B, 0x42, 0x08, 0xF6, 0x40, 0x38, 0x01 }; // 示例新数据
 
     if (WriteToMemory(processID, targetAddress, newData, sizeof(newData))) {
         std::wcout << L"内存修改成功！" << std::endl;
+        std::cout << "update ok!\n";
     }
 
-    return 0;
+    std::cout << "finished!\n";
+    //return 0;
 }
 
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
